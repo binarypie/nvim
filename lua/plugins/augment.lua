16c3032ea6488326853956d3a9c6ec32724229ee
binarypie <code@binarypie.com>
2025-09-26 10:05:08 -0700

    Remove augment chat and add hidden files

--- lua/plugins/augment.lua
+++ lua/plugins/augment.lua
@@ -1,9 +1,4 @@
 return {
   "augmentcode/augment.vim",
   cmd = "Augment",
-  keys = {
-    { "<leader>ac", "<cmd>Augment chat<cr>", desc = "Augment Chat" },
-    { "<leader>at", "<cmd>Augment chat-toggle<cr>", desc = "Augment Toggle" },
-    { "<leader>an", "<cmd>Augment chat-new<cr>", desc = "Augment New" },
-  },
 }
--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,6 +1,25 @@
 return {
   "snacks.nvim",
   opts = {
+    picker = {
+      files = {
+        sources = {
+          exclude = { "build/**", "target/**" },
+          hidden = true,
+          ignored = true,
+        },
+        grep = {
+          exclude = { "build/**", "target/**" },
+          hidden = true,
+          ignored = true,
+        },
+        explorer = {
+          exclude = { "build/**", "target/**" },
+          hidden = true,
+          ignored = true,
+        },
+      },
+    },
     dashboard = {
       -- dashboard configuration
       preset = {
